c17872953ab89c13f8232c945f69a2ffcfeb9571
Lars Tingelstad <lars.tingelstad@ntnu.no>
2019-03-25 15:00:02 +0100

    Added skeleton

---
+++ crane_hw_interface/include/crane_hw_interface/crane_hw_interface.h
@@ -0,0 +1,55 @@
+// Copyright (c) 2019 Norwegian University of Science and Technology
+// Use of this source code is governed by the MIT license, see LICENSE
+
+// Author: Lars Tingelstad (NTNU) <lars.tingelstad@ntnu.no>
+
+#ifndef CRANE_HW_INTERFACE
+#define CRANE_HW_INTERFACE
+
+#include <string>
+#include <vector>
+
+#include <boost/asio.hpp>
+
+#include <controller_manager/controller_manager.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <ros/ros.h>
+
+namespace crane_hw_interface
+{
+class CraneHardwareInterface : public hardware_interface::RobotHW
+{
+private:
+  ros::NodeHandle nh_;
+
+  const unsigned int n_dof_ = 6;
+  std::vector<std::string> joint_names_;
+  std::vector<double> joint_position_;
+  std::vector<double> joint_velocity_;
+  std::vector<double> joint_effort_;
+  std::vector<double> joint_position_command_;
+
+  // Timing
+  ros::Duration control_period_;
+  ros::Duration elapsed_time_;
+  double loop_hz_;
+
+  // Interfaces
+  hardware_interface::JointStateInterface joint_state_interface_;
+  hardware_interface::PositionJointInterface position_joint_interface_;
+
+public:
+  CraneHardwareInterface();
+  ~CraneHardwareInterface();
+
+  void init();
+  void start();
+  void read(const ros::Time &time, const ros::Duration &period);
+  void write(const ros::Time &time, const ros::Duration &period);
+};
+
+}  // namespace crane_hw_interface
+
+#endif  // CRANE_HW_INTERFACE
---
+++ crane_hw_interface/src/crane_hw_interface.cpp
@@ -0,0 +1,68 @@
+// Copyright (c) 2019 Norwegian University of Science and Technology
+// Use of this source code is governed by the MIT license, see LICENSE
+
+// Author: Lars Tingelstad (NTNU) <lars.tingelstad@ntnu.no>
+
+#include <crane_hw_interface/crane_hw_interface.h>
+
+namespace crane_hw_interface
+{
+CraneHardwareInterface::CraneHardwareInterface()
+  : joint_position_(n_dof_, 0.0)
+  , joint_velocity_(n_dof_, 0.0)
+  , joint_effort_(n_dof_, 0.0)
+  , joint_position_command_(n_dof_, 0.0)
+  , joint_names_(n_dof_)
+{
+}
+
+CraneHardwareInterface::~CraneHardwareInterface()
+{
+}
+
+
+void CraneHardwareInterface::init()
+{
+  // Get controller joint names from parameter server
+  if (!nh_.getParam("controller_joint_names", joint_names_))
+  {
+    ROS_ERROR("Cannot find required parameter 'controller_joint_names' on the parameter server.");
+    throw std::runtime_error("Cannot find required parameter 'controller_joint_names' on the parameter server.");
+  }
+
+  // Create ros_control interfaces (joint state and position joint for all dof's)
+  for (std::size_t i = 0; i < n_dof_; ++i)
+  {
+    // Joint state interface
+    joint_state_interface_.registerHandle(hardware_interface::JointStateHandle(joint_names_[i], &joint_position_[i],
+                                                                               &joint_velocity_[i], &joint_effort_[i]));
+
+    // Joint position control interface
+    position_joint_interface_.registerHandle(hardware_interface::JointHandle(
+        joint_state_interface_.getHandle(joint_names_[i]), &joint_position_command_[i]));
+  }
+
+  // Register interfaces
+  registerInterface(&joint_state_interface_);
+  registerInterface(&position_joint_interface_);
+
+  ROS_INFO_STREAM_NAMED("crane_hw_interface", "Loaded crane hardware interface");
+}
+
+void CraneHardwareInterface::start()
+{
+  ROS_INFO_NAMED("crane_hw_interface", "Starting crane hardware interface...");
+
+  ROS_INFO_NAMED("crane_hw_interface", "... done. crane hardware interface started!");
+}
+
+void CraneHardwareInterface::read(const ros::Time& time, const ros::Duration& period)
+{
+
+}
+
+void CraneHardwareInterface::write(const ros::Time& time, const ros::Duration& period)
+{
+}
+
+}  // namespace crane_hw_interface
