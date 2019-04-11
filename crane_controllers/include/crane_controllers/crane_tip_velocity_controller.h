935fbae5bbdc6d3d252fa75e8715f0bc7a3ce81d
Lars Tingelstad <lars.tingelstad@ntnu.no>
2019-04-11 06:23:03 -0700

    Started implementation of tip velocity controller

--- crane_controllers/include/crane_controllers/crane_tip_velocity_controller.h
+++ crane_controllers/include/crane_controllers/crane_tip_velocity_controller.h
@@ -6,15 +6,26 @@
 
 #include <boost/function.hpp>
 
+#include <ros/ros.h>
+
 #include <controller_interface/multi_interface_controller.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/robot_hw.h>
-#include <ros/package.h>
-#include <ros/node_handle.h>
-#include <ros/time.h>
-#include <std_msgs/Float64.h>
 #include <realtime_tools/realtime_publisher.h>
 #include <realtime_tools/realtime_buffer.h>
+
+#include <std_msgs/Float64.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+
+#include <kdl/chain.hpp>
+#include <kdl/chainiksolvervel_wdls.hpp>
+#include <kdl/chainfksolver.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/frames.hpp>
+
+#include <urdf/model.h>
+#include <kdl_parser/kdl_parser.hpp>
 
 namespace crane_controllers
 {
@@ -26,10 +37,30 @@ public:
   void starting(const ros::Time&) override;
   void update(const ros::Time&, const ros::Duration& period) override;
 
+  void command(const geometry_msgs::TwistStamped::ConstPtr& goal);
+
 private:
   hardware_interface::VelocityJointInterface* velocity_joint_interface_;
   std::vector<hardware_interface::JointHandle> velocity_joint_handles_;
+
+  bool initialized_;
+
+  KDL::Chain kdl_chain_;
+  boost::shared_ptr<KDL::ChainIkSolverVel_wdls> solver_;
+  boost::shared_ptr<KDL::ChainFkSolverPos_recursive> fksolver_;
+  KDL::JntArray tgt_jnt_pos_;
+  KDL::JntArray tgt_jnt_vel_;
+  KDL::JntArray last_tgt_jnt_vel_;
+
+  ros::Publisher feedback_pub_;
+  ros::Subscriber command_sub_;
+
+  boost::mutex mutex_;
+  KDL::Twist twist_command_;
+  std::string twist_command_frame_;
+  ros::Time last_command_time_;
+  bool is_active_;
 };
 }  // namespace crane_controllers
 
-PLUGINLIB_EXPORT_CLASS(crane_controllers::CraneController, controller_interface::ControllerBase)
+PLUGINLIB_EXPORT_CLASS(crane_controllers::CraneTipVelocityController, controller_interface::ControllerBase)
--- crane_controllers/src/crane_tip_velocity_controller.cpp
+++ crane_controllers/src/crane_tip_velocity_controller.cpp
@@ -12,6 +12,78 @@ namespace crane_controllers
 {
 bool CraneTipVelocityController::init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle)
 {
+  velocity_joint_interface_ = robot_hardware->get<hardware_interface::VelocityJointInterface>();
+  if (velocity_joint_interface_ == nullptr)
+  {
+    ROS_ERROR("CraneTipVelocityController: Error getting velocity joint interface from hardware!");
+    return false;
+  }
+
+  // Initialize KDL structures
+  std::string tip_link, root_link;
+  node_handle.param<std::string>("root_name", root_link, "base_link");
+  node_handle.param<std::string>("tip_name", tip_link, "tip_link");
+
+  // Load URDF
+  urdf::Model model;
+  if (!model.initParam("robot_description"))
+  {
+    ROS_ERROR("Failed to parse URDF");
+    return -1;
+  }
+
+  // Load the tree
+  KDL::Tree kdl_tree;
+  if (!kdl_parser::treeFromUrdfModel(model, kdl_tree))
+  {
+    ROS_ERROR("Could not construct tree from URDF");
+    return -1;
+  }
+
+  // Populate the Chain
+  if (!kdl_tree.getChain(root_link, tip_link, kdl_chain_))
+  {
+    ROS_ERROR("Could not construct chain from URDF");
+    return -1;
+  }
+
+  solver_.reset(new KDL::ChainIkSolverVel_wdls(kdl_chain_));
+  fksolver_.reset(new KDL::ChainFkSolverPos_recursive(kdl_chain_));
+  unsigned num_joints = kdl_chain_.getNrOfJoints();
+  tgt_jnt_pos_.resize(num_joints);
+  tgt_jnt_vel_.resize(num_joints);
+  last_tgt_jnt_vel_.resize(num_joints);
+
+  // Init Joint Handles
+  velocity_joint_handles_.clear();
+  for (size_t i = 0; i < kdl_chain_.getNrOfSegments(); ++i)
+  {
+    if (kdl_chain_.getSegment(i).getJoint().getType() != KDL::Joint::None)
+    {
+      velocity_joint_handles_.push_back(
+          velocity_joint_interface_->getHandle(kdl_chain_.getSegment(i).getJoint().getName()));
+    }
+  }
+
+  if (velocity_joint_handles_.size() != num_joints)
+  {
+    ROS_ERROR("Inconsistant joint count %d, %d", num_joints, int(velocity_joint_handles_.size()));
+    return false;
+  }
+
+  for (unsigned int i = 0; i < num_joints; ++i)
+  {
+    last_tgt_jnt_vel_(i) = 0.0;
+  }
+
+  // Subscribe to command
+  command_sub_ = node_handle.subscribe<geometry_msgs::TwistStamped>("command", 1,
+                                                           boost::bind(&CraneTipVelocityController::command, this, _1));
+  last_command_time_ = ros::Time(0);
+
+  initialized_ = true;
+
+  return true;
 }
 
 void CraneTipVelocityController::starting(const ros::Time&)
